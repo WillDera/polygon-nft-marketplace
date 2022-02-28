65963037b1d98efcc663f5ce1a9755a8e7d6846b
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-28 01:06:00 +0100

    Update creator-dashboard.js

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -8,6 +8,7 @@ import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
 export default function CreatorDashboard() {
+  const [sold, setSold] = useState([]);
   const [nfts, setNFTs] = useState([]);
   const [loadingState, setLoadingState] = useState("not-loaded");
   useEffect(() => {
@@ -26,7 +27,7 @@ export default function CreatorDashboard() {
       Market.abi,
       signer
     );
-    const data = await marketContract.fetchMyNFTs();
+    const data = await marketContract.fetchItemsCreated();
 
     const items = await Promise.all(
       data.map(async (i) => {
@@ -47,4 +48,66 @@ export default function CreatorDashboard() {
       })
     );
 
+    const soldItems = items.filter((i) => i.sold);
+    setSold(soldItems);
+    setNFTs(items);
+    setLoadingState("loaded");
+  }
+
+  return (
+    <div>
+      <div className="p-4">
+        <h2 className="text-2xl py-2">Items Created</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div
+              key={i}
+              className="border place-content-center shadow rounded-xl overflow-hidden"
+            >
+              <Image
+                alt={nft.name}
+                src={nft.image}
+                className="rounded "
+                height="300"
+                width="300"
+              />
+              <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">
+                  Price - {nft.price} Matic
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="px-4">
+        {Boolean(sold.length) && (
+          <div className="">
+            <h2 className="text-2xl py-2">Items sold</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+              {sold.map((nft, i) => (
+                <div
+                  key={i}
+                  className="border shadow rounded-xl overflow-hidden"
+                >
+                  <Image
+                    alt={nft.name}
+                    src={nft.image}
+                    className="rounded"
+                    height="250"
+                    width="200"
+                  />
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl font-bold text-white">
+                      Price - {nft.price} Matic
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
